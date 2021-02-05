dab01d3940dabb9764eb0a00d5c222683918d037
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-05 14:49:05 -0800

    initial add channel + playlist

---
+++ channel-metadata.sh
@@ -0,0 +1,26 @@
+##/bin/bash
+YT_DIR="/mnt/storage/media/youtube/"
+IFS=$'\n'
+count1=0
+count2=0
+
+cd "$YT_DIR"
+
+for i in $(find . -type f -name "*Channel Info*.info.json")
+do
+    noext=${i%.*.*}
+    if [[ -e "$noext.metadata" ]]; then
+        count1=$((count1+1))
+        :
+    else
+        cat "$i"  | jq -r '"[metadata]","title="+.title,"summary="+.description,"release=","studio="+.channel,"genres="+(.tags|join(",")),"collections="+.uploader,"actors="' > "$noext.metadata"
+        count2=$((count2+1))
+    fi
+done
+
+time=$(date +'%m/%d/%Y %r')
+
+echo "$time - $count2 metadata files were created and $count1 pre-existing files were skipped."
+
+unset IFS
+(.tags[]|tostring)
---
+++ playlist-metadata.sh
@@ -0,0 +1,25 @@
+##/bin/bash
+YT_DIR="/mnt/storage/media/youtube/"
+IFS=$'\n'
+count1=0
+count2=0
+
+cd "$YT_DIR"
+
+for i in $(find . -type f -name "*Playlist Info*.info.json")
+do
+    noext=${i%.*.*}
+    if [[ -e "$noext.metadata" ]]; then
+        count1=$((count1+1))
+        :
+    else
+        cat "$i"  | jq -r jq -r '"[metadata]","title="+.title,"summary=","release=","studio="+.channel,"genres=","collections="+.uploader,"actors="' > "$noext.metadata"
+        count2=$((count2+1))
+    fi
+done
+
+time=$(date +'%m/%d/%Y %r')
+
+echo "$time - $count2 metadata files were created and $count1 pre-existing files were skipped."
+
+unset IFS
