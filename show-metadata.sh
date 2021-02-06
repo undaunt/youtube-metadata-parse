36d9b27cfc8a5cf19aafe250e900a5d4ed1d9e4b
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-05 16:39:33 -0800

    combine playlist + channel into show

--- show-metadata.sh
+++ show-metadata.sh
@@ -6,7 +6,7 @@ count2=0
 
 cd "$YT_DIR"
 
-for i in $(find . -type f -name "*Channel Info*.info.json")
+for i in $(find . -type f -name "*Channel Info*.info.json" -o -name "*Playlist Info*.info.json*")
 do
     file=$(realpath "$i")
     folder=$(dirname "$file")
@@ -14,7 +14,7 @@ do
         count1=$((count1+1))
         :
     else
-        cat "$i"  | jq -r '"[metadata]","title="+.title,"summary="+.description,"release=","studio=YouTube","genres="+(.tags|join(",")),"collections="+.channel,"actors="+.uploader' > "$folder/show.metadata"
+        cat "$i"  | jq -r '"[metadata]","title="+.title,"summary="+.description,"release=","actors="+.uploader,"studio=YouTube","genres="+(.tags|join(",")),"collections="+.channel' > "$folder/show.metadata"
         count2=$((count2+1))
     fi
 done
