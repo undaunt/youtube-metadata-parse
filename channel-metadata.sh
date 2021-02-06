c45c03e77944b7fb5e0d1aaa31a26d7aeabb0c4d
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-05 16:19:01 -0800

    Update channel-metadata.sh

--- channel-metadata.sh
+++ channel-metadata.sh
@@ -8,13 +8,13 @@ cd "$YT_DIR"
 
 for i in $(find . -type f -name "*Channel Info*.info.json")
 do
-    file=$(realpath $i)
-    folder=$(dirname $file)
+    file=$(realpath "$i")
+    folder=$(dirname "$file")
     if [[ -e "show.metadata" ]]; then
         count1=$((count1+1))
         :
     else
-        cat "$i"  | jq -r '"[metadata]","title="+.title,"summary="+.description,"release=","studio=YouTube","genres="+(.tags|join(",")),"collections="+.channel,"actors="+.uploader' > "($folder/show.metadata)"
+        cat "$i"  | jq -r '"[metadata]","title="+.title,"summary="+.description,"release=","studio=YouTube","genres="+(.tags|join(",")),"collections="+.channel,"actors="+.uploader' > "$folder/show.metadata"
         count2=$((count2+1))
     fi
 done
