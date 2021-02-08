368a89c8f442310ebdaa6e56429e871cf98c8832
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-08 11:47:40 -0800

    Update youtube-playlist-thumbnail.sh

--- bin/youtube-playlist-thumbnail.sh
+++ bin/youtube-playlist-thumbnail.sh
@@ -11,19 +11,15 @@ for i in $(find . -type f \( -name "*Channel Info*.info.json" -o -name "*Playlis
 do
     file=$(realpath "$i")
     folder=$(dirname "$file")
-#    if [[ -e "$folder/poster.jpg" ]]; then
-#        count1=$((count1+1))
-#        :
-#    else
-        url1=$( jq -r '.thumbnails[] | select(.url|test("maxresdefault")) | .url' $i )
-        url2=$( jq -r '[.thumbnails[] | select(.url|test("hqdefault"))][0] | .url' $i )
-        shorturl=${url2%\?*}
-        echo "URL1 is $url1"
-        #echo "URL2 is $url2"
-        echo "Short URL is $shorturl"
-        #curl -o "$folder/poster.jpg" "$shorturl"
+    if [[ -e "$folder/poster.jpg" ]]; then
+        count1=$((count1+1))
+        :
+    else
+        url=$( jq -r '[.thumbnails[] | select(.url|test("hqdefault"))][0] | .url' $i )
+        shorturl=${url%\?*}
+        curl -o "$folder/poster.jpg" "$shorturl"
         count2=$((count2+1))
-#    fi
+    fi
 done
 
 time=$(date +'%m/%d/%Y %r')
@@ -31,3 +27,5 @@ time=$(date +'%m/%d/%Y %r')
 echo "$time - $count2 show metadata files were created and $count1 pre-existing files were skipped."
 
 unset IFS
+
+#        url1=$( jq -r '.thumbnails[] | select(.url|test("maxresdefault")) | .url' $i )
