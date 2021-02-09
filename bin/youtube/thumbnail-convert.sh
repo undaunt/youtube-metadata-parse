29d5fdd2ae88f4aeac64f1028bcab770622e2aba
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-09 11:55:19 -0800

    Update thumbnail-convert.sh

--- bin/youtube/thumbnail-convert.sh
+++ bin/youtube/thumbnail-convert.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 IFS=$'\n'
-count=0
+count1=0
+count2=0
 
 format_date() {
   date "+%m/%d/%Y %H:%M:%S"
@@ -15,14 +16,15 @@ do
     folder=$(dirname "$file")
     noext=${file%.*}
     if [[ -e "$noext.jpg" ]]; then
+        count1=$((count1+1))
         :
     else
         cp "$noext.webp" "$noext.jpg"
-        count=$((count+1))
+        count2=$((count2+1))
     fi
 done
 
-echo "$(format_date) - $count episode jpg posters were copied and renamed from webp."
+echo "$(format_date) - $count2 episode jpg posters were copied & renamed from webp. $count1 existing episode posters were skipped."
 echo
 #sleep 1
 
