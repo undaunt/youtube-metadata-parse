e398bb329cce73c57a4d97329c59b8ba2c181ac8
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-09 15:32:26 -0800

    Update series-metadata.sh

--- bin/youtube/series-metadata.sh
+++ bin/youtube/series-metadata.sh
@@ -3,6 +3,8 @@
 IFS=$'\n'
 count1=0
 count2=0
+count3=0
+count4=0
 
 format_date() {
   date "+%m/%d/%Y %H:%M:%S"
@@ -23,7 +25,31 @@ do
     fi
 done
 
-echo "$(format_date) - $count2 playist metadata files were created and $count1 pre-existing files were skipped."
+echo "$(format_date) - $count2 playlist metadata files were created and $count1 pre-existing files were skipped."
 echo
+
+for i in $(find . -type f -name "show.metadata")
+do
+    file=$(realpath "$i")
+    folder=$(dirname "$file")
+    cd "$folder"
+    for j in $(find . -type f -name "*S01E1 *.info.json" -o -name "*S01E01 *.info.json*" -o -name "*S01E001 *.info.json*" -o -name "*S01E0001 *.info.json*")
+    do
+        if grep -q "release=" show.metadata; then
+            count3=$((count3+1))
+            :
+        else
+            cat "$j"  | jq -r '"release="+.upload_date[0:4]+"-"+.upload_date[4:6]+"-"+.upload_date[6:8]' >> show.metadata
+            count4=$((count4+1))
+        fi
+        cd ..
+    done
+done
+
+echo "$(format_date) - $count4 series metadata files were appended with release dates while $count3 files were already complete."
+echo
+
+
+
 
 unset IFS
