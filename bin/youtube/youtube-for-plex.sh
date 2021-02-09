807a20cd153f5ed104988d93ae06d30379842c21
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-09 11:16:20 -0800

    sleep from 2 to 1

--- bin/youtube/youtube-for-plex.sh
+++ bin/youtube/youtube-for-plex.sh
@@ -22,13 +22,13 @@ playlist_check () {
     echo "$(format_date) - Some new playlist videos were downloaded."
     echo "$(format_date) - Looping through and executing the playlist job again to ensure there is no new content."
     echo
-    sleep 2
+    sleep 1
     playlist_dl
   else
     echo
     echo "$(format_date) - All playlist videos were already downloaded. Executing the youtube-dlc channels job."
     echo
-    sleep 2
+    sleep 1
     channel_dl
   fi
 }
@@ -38,35 +38,35 @@ channel_check () {
     echo "$(format_date) - Some new channel videos were downloaded."
     echo "$(format_date) - Looping through and executing the channels job again to ensure there is no new content."
     echo
-    sleep 2
+    sleep 1
     channel_dl
   else
     echo
     echo "$(format_date) - All playlist and channel videos have been downloaded and are up to date."
     echo
     echo "$(format_date) - Executing the episode metadata job. This may take a few moments if a large number of videos have just been downloaded."
-    sleep 2
+    sleep 1
   fi
 }
 
 echo
 echo "$(format_date) - Beginning the youtube-dlc download and metadata parse jobs."
-sleep 2
+sleep 1
 echo "$(format_date) - Executing the youtube-dlc playlist job."
 echo
-sleep 2
+sleep 1
 
 playlist_dl
 
 "$STORAGEDIR/bin/youtube/episode-metadata.sh"
 
 echo "$(format_date) - Executing the show metadata job."
-sleep 2
+sleep 1
 
 "$STORAGEDIR/bin/youtube/series-metadata.sh"
 
 echo "$(format_date) - Executing the playlist thumbnail job."
-sleep 2
+sleep 1
 
 "$STORAGEDIR/bin/youtube/playlist-thumbnails.sh"
 
