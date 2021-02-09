a7bd887ab0e91d540e44b5785c6f40c17dc3fa03
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-09 12:21:34 -0800

    add hardlink series poster

--- bin/youtube/thumbnail-convert.sh
+++ bin/youtube/thumbnail-convert.sh
@@ -3,6 +3,7 @@
 IFS=$'\n'
 count1=0
 count2=0
+titlecount=1
 
 format_date() {
   date "+%m/%d/%Y %H:%M:%S"
@@ -25,6 +26,25 @@ do
 done
 
 echo "$(format_date) - $count2 episode jpg posters were copied & renamed from webp. $count1 existing episode posters were skipped."
+echo
+
+for i in $(find . -type f -name "*.jpg" -not -name "poster.jpg")
+do
+    file=$(realpath "$i")
+    folder=$(dirname "$file")
+    noext=${file%.*}
+    if [[ ! -e "poster-$titlecount.jpg" ]]; then # if poster-1 doesn't exist
+        ln "$noext.jpg" "poster-$titlecount.jpg" # hardlink EP1.poster to poster-1.jpg
+        titlecount=$((titlecount+1)) # bump title to 2
+    else
+        count3=$((count3+1))
+        :
+    fi
+done
+
+finalcount=$((titlecount-1))
+
+echo "$(format_date) - $finalcount episode posters were hardlinked to series posters. $count3 existing series posters were skipped."
 echo
 
 unset IFS
