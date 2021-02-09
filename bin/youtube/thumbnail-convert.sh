eeb1b9e1604e6a5226e86e7efca6c95b0d2e7c65
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-09 14:05:09 -0800

    Update thumbnail-convert.sh

--- bin/youtube/thumbnail-convert.sh
+++ bin/youtube/thumbnail-convert.sh
@@ -31,7 +31,7 @@ echo
 
 for d in $(find . -type d -name "* - [PL*" -o -name "* - Videos - [UC*")
 do
-    folder2=$(realpath "$d")
+    folder2=$(basename "$d")
     echo "d = $d"
     echo
     echo "folder is $folder2"
