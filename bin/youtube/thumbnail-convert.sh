8083ad45bbbcf38e172a081d92eaa7d130aa4409
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-09 13:57:28 -0800

    Update thumbnail-convert.sh

--- bin/youtube/thumbnail-convert.sh
+++ bin/youtube/thumbnail-convert.sh
@@ -29,7 +29,7 @@ cd "$MEDIADIR/youtube"
 #echo "$(format_date) - $count2 webp episode posters were hardlinked to jpg. $count1 existing episode posters were skipped."
 #echo
 
-for d in $(find . -type d -name "* - [PL*" -o -name "* - Videos - [UC*" -execdir {} \;)
+for d in $(find . -type d -name "* - [PL*" -o -name "* - Videos - [UC*")
 do
     folder=$(basename "$d")
     echo "d = $d"
