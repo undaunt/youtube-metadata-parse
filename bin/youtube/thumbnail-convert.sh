a6d32cf00973dad81280853483c918380f6f754d
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-09 13:32:55 -0800

    Update thumbnail-convert.sh

--- bin/youtube/thumbnail-convert.sh
+++ bin/youtube/thumbnail-convert.sh
@@ -32,7 +32,7 @@ echo
 for d in $(find . -type d -name "* - [PL*" -o -name "* - Videos - [UC*")
 do
     echo "d is $d"
-    folder=$(realpath "$d")
+    local folder=$(realpath "$d")
     echo "folder is $folder"
     cd "$folder"
     for i in $(find . -type f -name "*.jpg" -not -name "poster*.jpg" -exec ls {} + | sort -k 10)
