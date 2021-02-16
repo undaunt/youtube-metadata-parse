cbc98fa6065293c2a729d19e917048de5add3b91
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-16 11:34:50 -0800

    Add spacing check for descriptions metadata

--- bin/01-episode-metadata.sh
+++ bin/01-episode-metadata.sh
@@ -3,6 +3,9 @@
 IFS=$'\n'
 count1=0
 count2=0
+count3=0
+count4=0
+count5=0
 
 format_date() {
   date "+%m/%d/%Y %H:%M:%S"
@@ -16,8 +19,8 @@ for i in $(find . -type f -name "*.info.json" -not -name "*Channel Info*" -not -
 do
     noext=${i%.*.*}
     if [[ -e "$noext.metadata" ]]; then
-        count1=$((count1+1))
-        :
+      count1=$((count1+1))
+      :
     else
         cat "$i"  | jq -r '"[metadata]","title="+.title,"summary="+.description,"release="+.upload_date[0:4]+"-"+.upload_date[4:6]+"-"+.upload_date[6:8],"directors="+.uploader' > "$noext.metadata"
         count2=$((count2+1))
@@ -25,6 +28,31 @@ do
 done
 
 echo "$(format_date) - $count2 episode metadata files were created and $count1 pre-existing files were skipped."
+echo
+
+for i in $(find . -type f -name "*.metadata" -not -name "show.metadata")
+do
+    releaseline=$(awk '/release=/{ print NR; exit }' "$i")
+    if [[ releaseline -gt 4 ]]; then
+      startline=4
+      endline=$((releaseline-1))
+      status=$(awk '/^[[:blank:]]/' "$i")
+      if [[ -z "$status" ]]; then
+        for (( j=$startline; j<=$endline; j++ )); do
+          sed -i '"$j"s/^/\ /' "$i"
+        done
+        count3=$((count3+1))
+      else
+        count4=$((count4+1))
+        :
+      fi
+    else
+      count5=$((count5+1))
+      :
+    fi
+done
+
+echo "$(format_date) - $count3 episode descriptions were indented, $count4 episodes' descriptions were previously indented, and $count5 episodes have single line descriptions."
 echo
 
 unset IFS
