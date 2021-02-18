4687763007a7ab9c8419a972e2cc1d653e3cbb4b
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-18 12:13:32 -0800

    tabbing fixed

--- bin/01-episode-metadata.sh
+++ bin/01-episode-metadata.sh
@@ -17,38 +17,35 @@ cd "$MEDIADIR/youtube"
 
 for i in $(find . -type f -name "*.info.json" -not -name "*Channel Info*" -not -name "*Playlist Info*")
 do
-    noext=${i%.*.*}
-    if [[ -e "$noext.metadata" ]]; then
-      count1=$((count1+1))
-      :
-    else
-        cat "$i"  | jq -r '"[metadata]","title="+.title,"summary="+.description,"release="+.upload_date[0:4]+"-"+.upload_date[4:6]+"-"+.upload_date[6:8],"directors="+.uploader' > "$noext.metadata"
-        count2=$((count2+1))
-    fi
+  noext=${i%.*.*}
+  if [[ -e "$noext.metadata" ]]; then
+    count1=$((count1+1))
+  else
+    cat "$i"  | jq -r '"[metadata]","title="+.title,"summary="+.description,"release="+.upload_date[0:4]+"-"+.upload_date[4:6]+"-"+.upload_date[6:8],"directors="+.uploader' > "$noext.metadata"
+    count2=$((count2+1))
+  fi
 done
 
 echo "$(format_date) - $count2 episode metadata files were created and $count1 pre-existing files were skipped."
 
 for i in $(find . -type f -name "*.metadata" -not -name "show.metadata")
 do
-    releaseline=$(awk '/release=/{ print NR; exit }' "$i")
-    if [[ releaseline -gt 4 ]]; then
-      startline=4
-      endline=$((releaseline-1))
-      status=$(awk '/^[[:blank:]]/' "$i")
-      if [[ -z "$status" ]]; then
-        for (( j=$startline; j<=$endline; j++ )); do
-          sed -i "${j}s/^/\ /" "$i"
-        done
-        count3=$((count3+1))
-      else
-        count4=$((count4+1))
-        :
-      fi
+  releaseline=$(awk '/release=/{ print NR; exit }' "$i")
+  if [[ releaseline -gt 4 ]]; then
+    startline=4
+    endline=$((releaseline-1))
+    status=$(awk '/^[[:blank:]]/' "$i")
+    if [[ -z "$status" ]]; then
+      for (( j=$startline; j<=$endline; j++ )); do
+        sed -i "${j}s/^/\ /" "$i"
+      done
+      count3=$((count3+1))
     else
-      count5=$((count5+1))
-      :
+      count4=$((count4+1))
     fi
+  else
+    count5=$((count5+1))
+  fi
 done
 
 echo "$(format_date) - $count3 episode descriptions were indented, $count4 episodes' descriptions were previously indented, and $count5 episodes have single line descriptions."
