7bb3fca24411899687b6ad0af62720be0951498e
undaunt <31376520+undaunt@users.noreply.github.com>
2021-02-18 12:03:26 -0800

    cleanup find with pwd

--- bin/04-thumbnail-link.sh
+++ bin/04-thumbnail-link.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+oi9#!/bin/bash
 
 IFS=$'\n'
 count1=0
 count2=0
 count3=0
+count4=0
 titlecount=1
-newcount=0
 
 format_date() {
   date "+%m/%d/%Y %H:%M:%S"
@@ -13,14 +13,11 @@ format_date() {
 
 cd "$MEDIADIR/youtube"
 
-for i in $(find . -type f -name "*.webp")
+for i in $(find "$(pwd)" -type f -name "*.webp")
 do
-    file=$(realpath "$i")
-    folder=$(dirname "$file")
-    noext=${file%.*}
+    noext=${i%.*}
     if [[ -e "$noext.jpg" ]]; then
         count1=$((count1+1))
-        :
     else
         ln "$noext.webp" "$noext.jpg"
         count2=$((count2+1))
@@ -32,15 +29,13 @@ echo "$(format_date) - $count2 webp episode posters were hardlinked to jpg. $cou
 for d in $(find . -type d -name "* - [PL*" -o -name "* - Videos - [UC*")
 do
     cd "$d"
-    for i in $(find . -type f -name "*.jpg" -not -name "poster*.jpg" -exec ls {} +)
+    for i in $(find "$(pwd)" -type f -name "*.jpg" -not -name "poster*.jpg" -exec ls {} +)
     do
-        file=$(realpath "$i")
         if [[ ! -e "poster-$titlecount.jpg" ]]; then
-            ln "$file" "poster-$titlecount.jpg"
-            newcount=$((newcount+1))
+            ln "$i" "poster-$titlecount.jpg"
+            count4=$((count4+1))
         else
             count3=$((count3+1))
-            :
         fi
         titlecount=$((titlecount+1))
     done
@@ -48,7 +43,7 @@ do
     cd ..
 done
 
-echo "$(format_date) - $newcount episode posters were hardlinked to series posters. $count3 pre-existing series posters were skipped."
+echo "$(format_date) - $count4 episode posters were hardlinked to series posters. $count3 pre-existing series posters were skipped."
 echo
 
 unset IFS
